fb8f6b347f3ac597a7d7645be73f92b71139c2b0
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2023-12-10 22:53:21 +0700

    first commit

---
+++ src/main/java/com/kiotfpt/Kiotfpt_Backend/KiotfptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kiotfpt.Kiotfpt_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiotfptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiotfptBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiotfpt/Kiotfpt_Backend/KiotfptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiotfpt.Kiotfpt_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiotfptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
